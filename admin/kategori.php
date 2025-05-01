f9937a8e3b79e79195da3d8de87541ee9c2bb5e6
firlyfahriza <firlyfahriza@yahoo.com>
2025-05-01 21:53:02 +0700

    menyesuaikan footer dengan mengubah nama web dan pembuat dan link ig

--- admin/kategori.php
+++ admin/kategori.php
@@ -256,14 +256,14 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>electrons</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      Designed by <a href="https://instagram.com/firlyfahriza" target="_blank">FirlyFahriza</a>
     </div>
   </footer><!-- End Footer -->
 
