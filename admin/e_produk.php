72d6031981ca9a55a188c916f211c4da64153567
firlyfahrizaa <firlyfahriza@yahoo.com>
2025-05-22 20:52:55 +0700

    membenarkan e_produk.php

--- admin/e_produk.php
+++ admin/e_produk.php
@@ -73,7 +73,7 @@ if (isset($_POST['update'])) {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Produk - Nama Website Admin</title>
+    <title>Produk - electrons Admin</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -221,7 +221,7 @@ if (isset($_POST['update'])) {
 
         <section class="section">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-body">
                             <!-- Vertical Form -->
@@ -229,7 +229,7 @@ if (isset($_POST['update'])) {
                                 <input type="hidden" name="gambar_lama" value="<?php echo $data['gambar']; ?>">
                                 <div class="col-12">
                                     <label for="nm_produk" class="form-label">Nama Produk</label>
-                                    <input type="text" class="form-control" id="nm_produk" name="nm_produk" placeholder="Masukkan Nama Produk" required>
+                                    <input type="text" class="form-control" id="nm_produk" name="nm_produk" placeholder="Masukkan Nama Produk" value="<?php echo $data['nm_produk']; ?>" required>
                                 </div>
                                 <div class="col-12">
                                     <label for="harga" class="form-label">Harga</label>
@@ -259,6 +259,10 @@ if (isset($_POST['update'])) {
                                 <div class="col-12">
                                     <label for="gambar" class="form-label">Gambar Produk</label>
                                     <input type="file" class="form-control" id="gambar" name="gambar" accept="image/*">
+                                    <br>
+                                    <?php if ($data['gambar']) { ?>
+                                        <img src="produk_img/<?php echo $data['gambar']; ?>" width="150">
+                                    <?php } ?>
                                 </div>
                                 <div class="text-center">
                                     <button type="reset" class="btn btn-secondary">Reset</button>
